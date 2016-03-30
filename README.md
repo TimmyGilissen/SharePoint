caf0e1d283f99a557a7e235d04440c5c95cf0c4c
TimmyGilissen <timmy.gilissen@gilit.be>
2016-03-30 14:24:58 +0200

    Taxonomy topology refiner

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Building a personal SharePoint snippit repository.
 
 Feel free to use and fork where needed.
+
+- Taxonomy Topology refiner
---
+++ SharePoint/Search/refinments/TaxonomyTopology/Taxonomy-Refinment-Topology.js
@@ -0,0 +1,66 @@
+﻿function execOperation(refiner, termId, termName) {
+
+    if (termId.length === 37) {
+        termId = termId.substr(1);
+    }
+
+    //Current Context
+    var context = SP.ClientContext.get_current();
+
+    //Current Taxonomy Session
+    var taxSession = SP.Taxonomy.TaxonomySession.getTaxonomySession(context);
+
+    //Term Stores
+    var termStores = taxSession.get_termStores();
+
+    //Name of the Term Store from which to get the Terms.
+    var termStore = termStores.getByName("Managed Metadata Service Application");
+
+    //GUID of Term Set from which to get the Terms.
+    var termSet = termStore.getTerm(termId);
+
+    var terms = termSet.get_terms();
+
+    context.load(termSet);
+    context.load(terms);
+
+    context.executeQueryAsync(function () {
+
+        var termEnumerator = terms.getEnumerator();
+
+        var termList = new Object();
+
+        termList[termName] = new Array();
+
+        termList[termName].push('string("L0|#0' + termSet.get_id() + '|' + termSet.get_name() + '")');
+
+        while (termEnumerator.moveNext()) {
+
+            var currentTerm = termEnumerator.get_current();
+
+            termList[termName].push('string("L0|#0' + currentTerm.get_id() + '|' + currentTerm.get_name() + '")');
+        }        
+
+        refiner.addRefinementFiltersJSON(Sys.Serialization.JavaScriptSerializer.serialize(termList));
+
+    }, function (sender, args) {
+
+    });
+
+}
+
+$(document).ready(function () {
+    var scriptbase = _spPageContextInfo.siteAbsoluteUrl + "/_layouts/15/";
+
+    $.ajax({
+        async: false,
+        url: scriptbase + "SP.js",
+        dataType: "script"
+    });
+
+    $.ajax({
+        async: false,
+        url: scriptbase + "SP.Taxonomy.js",
+        dataType: "script"
+    });
+});
